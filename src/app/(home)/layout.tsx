d24b5ac8b5be36caed4d8fd68df7f8e93f500056
BaeejiH <bjihun0511@gmail.com>
2024-06-24 16:01:39 +0900

    레이아웃 수정

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -27,13 +27,13 @@ export const viewport: Viewport = {
 /* meta data */
 export const metadata: Metadata = {
 	metadataBase: new URL('https://seungyn.com'),
-	title: '프론트엔드 유승윤 | 포트폴리오',
+	title: '백엔드 배지훈 | 포트폴리오',
 	description:
-		'프론트엔드 개발자 유승윤 포트폴리오, seungyn frontend portfolio',
+		'백엔드 개발자 배지훈 포트폴리오, seungyn frontend portfolio',
 	keywords:
 		'프론트엔드,리액트, 자바스크립트,타입스크립트, 리액트쿼리, 주스탄드,스프링,포트폴리오,넥스트,frontend, React,nextjs, typescript, javascript, typescript, zustant, react-query',
 	openGraph: {
-		title: '프론트엔드 유승윤 | 포트폴리오',
+		title: '백엔드 배지훈 | 포트폴리오',
 		type: 'website',
 		siteName: 'seungyn',
 		images: '/assets/og/ogImage.png',
